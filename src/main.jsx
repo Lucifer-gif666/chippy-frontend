65fbda656ef3b81967e97b233c8262bfccaa0b9d
Prem-Rajeevan <premrajeevan027@gmail.com>
2025-12-16 12:55:26 +0530

    axiosPath imported in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import "./api/axiosPatch";
 
 // create root once
 const root = createRoot(document.getElementById("root"));
